b36c2ab6fd03dd0348575d7c87531a514a26cf7a
wtraceback <whxcer@outlook.com>
2021-05-03 17:56:35 +0800

    添加新话题的 model 类以及完成添加操作

---
+++ models/topic.py
@@ -0,0 +1,16 @@
+import time
+
+from models import Model
+
+
+class Topic(Model):
+    """
+    话题类
+    """
+    def __init__(self, form):
+        self.id = form.get('id', None)
+        self.title = form.get('title', '')
+        self.content = form.get('content', '')
+        self.ct = int(time.time())
+        self.ut = self.ct
+        self.user_id = form.get('user_id', '')
--- routes/topic.py
+++ routes/topic.py
@@ -2,7 +2,11 @@ from flask import (
     Blueprint,
     render_template,
     request,
+    redirect,
+    url_for,
 )
+from routes import current_user
+from models.topic import Topic
 
 
 topic = Blueprint('topic', __name__)
@@ -16,6 +20,9 @@ def index():
 @topic.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        pass
+        form = request.form
+        u = current_user()
+        t = Topic.new(form, user_id=u.id)
+        return redirect(url_for('topic.index'))
 
     return render_template('topic/add.html')
